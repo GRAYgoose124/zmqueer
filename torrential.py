833541a301470e925989fe0277be593ba423a599
Grayson <grayson.miller124@gmail.com>
2023-08-05 15:50:57 -0600

    sanity maybe

--- torrential.py
+++ torrential.py
@@ -3,7 +3,12 @@ from enum import Enum
 from pathlib import Path
 from dataclasses import dataclass
 import lzma
-
+import random
+import tarfile
+from typing import Any
+import uuid
+import logging
+import os
 
 from zmqer.peer import TaskablePeer
 from zmqer.misc import connect_all
@@ -21,39 +26,57 @@ class Package:
         self._archive: Path = self.path.parent / f"{self.name}.tar.gz"
         # Allow the archive to be invalidated if the package is modified. We set this to true for first run, but you can change this if you know what you're doing.
         self.__can_invalidate_archive: bool = True
-        self.__archive_needs_update: bool = False
+        self.__force_archive_needs_update: bool = True
+        self.__held_by: list[str] = []
 
     @property
     def archive(self):
-        if self.__archive_needs_update() and self.__can_invalidate_archive:
-            self.__update_archive()
-            # This should only be enabled when we know there are no active transactions using Package.
-            self.__can_invalidate_archive = False
+        if self._archive_needs_update() and self.can_invalidate_archive:
+            self._update_archive()
 
         return self._archive
 
-    def __update_archive(self):
+    @property
+    def can_invalidate_archive(self):
+        """Set whether the archive can be invalidated."""
+        self.__can_invalidate_archive = not self.__held_by
+
+        return self.__can_invalidate_archive
+
+    def _update_archive(self):
         """Update the archive of the package."""
-        # save it next to the package root as a tar.gz. We could stream, but this is more convenient.
-        with open(self._archive, "wb") as archive:
-            archive.write(lzma.compress(self.path.read_bytes()))
+        xz_file = lzma.LZMAFile(self._archive, "w")
+        with tarfile.open(mode="w", fileobj=xz_file) as tar_xz_file:
+            tar_xz_file.add(str(self.path.resolve()))
 
-    def __check_archive_age(self) -> int:
+        xz_file.close()
+
+        self.__force_archive_needs_update = False
+
+    def _check_archive_age(self) -> int:
         """Check the age of the archive in seconds."""
-        return (self._archive.stat().st_mtime - self.path.stat().st_mtime).seconds
+        try:
+            return (self._archive.stat().st_mtime - self.path.stat().st_mtime).seconds
+        except FileNotFoundError:
+            self.__force_archive_needs_update = True
+            return 0
 
-    def __archive_needs_update(self) -> bool:
+    def _archive_needs_update(self) -> bool:
         """Check if the archive needs updating by comparing the modified times of the archive and the package."""
-        return self.__check_archive_age() > 0 or self.__archive_needs_update
+        return self._check_archive_age() > 0 or self.__force_archive_needs_update
 
     def stream_iter(self, chunk_size: int = 1024) -> bytes:
         """Return a byte stream of the package."""
+        self.__held_by.append("stream_iter")
+
         with open(self.archive, "rb") as archive:
             while True:
                 chunk = archive.read(chunk_size)
                 if not chunk:
                     break
                 yield chunk
+
+        self.__held_by.remove("stream_iter")
 
     def __len__(self):
         """Return the size of the package in bytes."""
@@ -73,6 +96,7 @@ class Transaction:
         complete = 3
         failed = 4
 
+    ID: str = uuid.uuid4().hex
     status: Status = Status.not_started
     package: Package = None
     recipient: str = None
@@ -84,21 +108,11 @@ class TorrentialPeer(TaskablePeer):
         super().__init__(*args, **kwargs)
         self.transactions = {}
 
-    async def broadcast(self, type: str, package: Package):
-        # Create a new transaction and register it
-        transaction = Transaction(
-            status=Transaction.Status.pending,
-            package=package,
-            recipient=self.address,
-            providers=[self.address],
-        )
-        self.transactions[package.name] = transaction
-
-        # Start the transaction
-        await self.start_transaction(transaction)
-
     async def start_transaction(self, transaction: Transaction):
         transaction.status = Transaction.Status.transmitting
+        self.logger.info(
+            f"Transaction {transaction.ID} started for package {transaction.package.name}"
+        )
         package_stream = transaction.package.stream_iter()
 
         for chunk in package_stream:
@@ -107,6 +121,9 @@ class TorrentialPeer(TaskablePeer):
             self.logger.debug(f"{self.address}:\n\tSent message: {packet}")
 
         transaction.status = Transaction.Status.complete
+        self.logger.info(
+            f"Transaction {transaction.ID} completed for package {transaction.package.name}"
+        )
 
     async def message_type_handler(self, message):
         package_name, chunk = message.split("=")
@@ -118,25 +135,68 @@ class TorrentialPeer(TaskablePeer):
                 recipient=self.address,
                 providers=[self.address],
             )
+            self.logger.info(
+                f"New transaction {self.transactions[package_name].ID} created for package {package_name}"
+            )
+
         transaction = self.transactions[package_name]
         transaction.status = Transaction.Status.transmitting
+        self.logger.info(
+            f"Transaction {transaction.ID} started for package {transaction.package.name}"
+        )
         transaction.package.path.write_bytes(chunk)
 
         if len(transaction.package) >= len(chunk):
             transaction.status = Transaction.Status.complete
+            self.logger.info(
+                f"Transaction {transaction.ID} completed for package {transaction.package.name}"
+            )
         else:
             transaction.status = Transaction.Status.pending
+            self.logger.info(
+                f"Transaction {transaction.ID} still pending for package {transaction.package.name}"
+            )
+
+    def handle_completed_task(self, data: dict[str, Any]):
+        print(f"Completed task, results: {data=}")
 
 
 def main():
-    peers = [TorrentialPeer(f"tcp://localhost:{port})") for port in range(5555, 5565)]
+    logging.basicConfig(level=logging.DEBUG)
+
+    if os.name == "nt":
+        from asyncio import WindowsSelectorEventLoopPolicy
+
+        asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+
+    start_port = 5555 + random.randint(0, 1000)
+    peers = [
+        TorrentialPeer(f"tcp://127.0.0.1:{port}")
+        for port in range(start_port, start_port + 10)
+    ]
     peers[0].packages = [Package(name="test", path=Path("tests"))]
 
     connect_all(peers)
 
     try:
         loop = asyncio.get_event_loop()
-        fut = asyncio.gather(*[peer.run() for peer in peers])
+
+        tasks = set()
+        for peer in peers:
+            tasks.update(peer.setup())
+
+        # add a custom start transaction task, providers must be part of the same group as recipient
+        tasks.add(
+            peers[0].start_transaction(
+                Transaction(
+                    package=Package(name="test", path=Path("./tests").resolve()),
+                    providers=[peers[0].address],
+                    recipient=peers[1].address,
+                )
+            )
+        )
+
+        fut = asyncio.gather(*tasks)
         loop.run_until_complete(fut)
     except KeyboardInterrupt:
         print("Exiting...")
@@ -146,3 +206,7 @@ def main():
         )
     finally:
         loop.close()
+
+
+if __name__ == "__main__":
+    main()
